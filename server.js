da648925f8fea3d35c41b40a9b611f880df33726
mrSaran29 <saran2962002@gmail.com>
2025-11-17 12:57:33 +0530

    added morgan

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const CrudRouter = require('./Router/router');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
 
 
 dotenv.config();
+
 
 
 mongoose.set('strictQuery', false);
@@ -15,7 +17,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DB_STRING, { serverSelectionTimeoutMS: 5000 })
   .then(() => console.log("✅ Database Connected Successfully"))
   .catch(err => console.log("❌ Database Connection Error:", err.message));
-
+app.use(morgan('dev'))
 app.use(cors());
 app.use(express.json());
 app.use('/home', CrudRouter);
